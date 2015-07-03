328291ce09550caccea0dbd789579a6888b5bb6c
Vitalii Petrychuk <vpetrychuk@atlassian.com>
2015-07-03 21:06:53 +0200

    add get/add/save methods

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 React Native AsyncStorage (WIP)
 -------------------------------
 
-An abstraction on top of react-native's AsyncStorage
+An abstraction on top of react-native's AsyncStorage.
+
+####TODO
+ - Tests
+ - update(id:number, data:Object), remove(id:number|Array), clear(), find(criteria:Object)
+
+####Simple example
+
+    ReactNativeAsyncStorage.using('model_name').then((model) => {
+      model.get(id);
+      model.find();
+      model.add({field1: 'value'});
+      model.add([{field2: 'value'}, {field3: 'value'}]);
+      model.save().then(() => {
+        console.log('Model has been saved');
+      });
+    });
---
+++ src/model.js
@@ -0,0 +1,38 @@
+'use strict';
+
+class Model {
+  constructor(data, save) {
+    try {
+      this.data = JSON.parse(data);
+    }
+    catch (e) {
+      throw new Error('Invalid data');
+    }
+    this.save = () => save(this.data);
+    this.nextId = Number(Object.keys(this.data)[0]) || -1 + 1;
+  }
+
+  get(id) {
+    return this.data[id];
+  }
+
+  add(entries) {
+    if (!Array.isArray(entries)) {
+      entries = [entries];
+    }
+
+    return entries.map((entry) => {
+      var id = entry._id = this.nextId;
+      this.data[id] = entry;
+      this.nextId += 1;
+      return id;
+    });
+  }
+
+  find() {
+    return Object.keys(this.data).map(key => this.data[key]);
+  }
+
+}
+
+module.exports = Model;
